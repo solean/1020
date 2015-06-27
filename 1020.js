b7c5b472c4addf183d0e50c958d6db63d7ec5c19
solean <he15man2007@gmail.com>
2015-06-27 15:32:50 -0400

    Added some styling, calculated avg

--- 1020.js
+++ 1020.js
@@ -3,13 +3,13 @@ var chartLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
              "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34",
              "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45",
              "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56",
-             "57", "58", "59", "60", "61", "62", "63", "64", "65", "66"];
+             "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67"];
 var chartData = [1, 12, 21, 47, 61, 79, 88, 95, 99, 59, 75, 63, 48, 85, 35,
                 126, 86, 91, 120, 90, 108, 113, 73, 84, 84, 111, 100, 112,
                 143, 123, 150, 137, 147, 157, 137, 133, 153, 176, 205, 242,
                 242, 222, 219, 231, 234, 245, 262, 265, 276, 322, 349, 351,
                 355, 351, 356, 404, 389, 369, 372, 376, 385, 385, 399, 392,
-                393, 400];
+                393, 400, 417];
 
 
 var data = {
@@ -19,9 +19,9 @@ var data = {
             label: "1020",
             fillColor: "rgba(220,220,220,0.2)",
             // strokeColor: "rgba(220,220,220,1)",
-            strokeColor: "#0080FF",
+            strokeColor: "#F5A623",
             // pointColor: "rgba(220,220,220,1)",
-            pointColor: "#00FFFF",
+            pointColor: "#fff",
             pointStrokeFill: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
@@ -33,7 +33,7 @@ var dataLength = data.datasets[0].data.length;
 
 var canvas = document.getElementById("myChart");
 var browserWidth = window.innerWidth;
-canvas.style.width = browserWidth;
+canvas.style.width = (browserWidth - 30) + "px";
 
 var ctx = canvas.getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {});
@@ -41,5 +41,9 @@ var myLineChart = new Chart(ctx).Line(data, {});
 
 currentTotal = chartData[chartData.length - 1];
 var total = document.getElementById("total");
-total.innerHTML = currentTotal + " bones";
+total.innerHTML = "$" + currentTotal + " total"
 
+avgPerSession = currentTotal / dataLength;
+avgInBB = avgPerSession * 5;
+var avg = document.getElementById("avg");
+avg.innerHTML = "$" + avgPerSession.toFixed(2) + " per (" + avgInBB.toFixed(2) + " BB)";
